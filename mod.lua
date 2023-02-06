0b5c4010f710161f33367950ccfdf8381183b975
lolluslollus <lolluslollus@users.noreply.github.com>
2023-02-06 12:03:51 +0100

    Initial commit

---
+++ mod.lua
@@ -0,0 +1,25 @@
+function data()
+	return {  
+		info = {
+			tags = { "SNCF", "Mur", "France", "Wall", "Retaining", "Massif Central", "Tunnel", "PLM", "PO",},
+			minorVersion = 2,
+			severityAdd = "NONE",
+			severityRemove = "CRITICAL",
+			name = _("Murs de soutènement style français - Portails de Tunnels"),
+			description = _("Pack rassemblant différents murs de soutènement et portails de tunnels dans des teintes inspirées par les lignes du Massif central, basé sur le mod de Lenz"),
+			authors = {
+				{
+					name = "Lenz",
+					role = "CREATOR",
+					name = "Loco141R",
+					role = "CO_CREATOR",
+				},
+			},
+			visible = true,
+		},
+		options = {
+		},
+		runFn = function (settings)		
+		end
+	}
+end
